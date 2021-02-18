e2e60cbd54a7199e14e9befedc70ccd6dd612a5f
Abel AR <abel_john_14@hotmail.com>
2021-02-18 11:42:34 -0500

    request API and solve problems

--- useeffect-app/src/FetchCard.js
+++ useeffect-app/src/FetchCard.js
@@ -1,13 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import getPost from './helpers/getPost';
 import getUser from './helpers/getUser';
 
-const initialUser = {
-    name: "Regulus",
-    email: "regulus@mail.com",
-}
+// const initialUser = {
+//     name: "Regulus",
+//     email: "regulus@mail.com",
+// };
+
+// const initialPost = [
+//     {
+//         id: 1,
+//         title: "Medicim",
+//         developer: "full stack",
+//     },
+
+//     {
+//         id: 2,
+//         title: "Food",
+//         developer: "Fron end",
+//     },
+// ]
 
 function FetchCard() {
-    const [user, setUser] = useState(initialUser);
+    const [user, setUser] = useState({});
+    const [posts, setPosts] = useState([]);
 
     const updateUser = () => {
         getUser()
@@ -16,9 +32,22 @@ function FetchCard() {
             })
     }
 
+    const updatePost = useCallback(() => {
+        getPost(user.id)
+            .then((newPosts) => {
+                setPosts(newPosts);
+            } )
+    }, [user.id]);
+
     useEffect( () => {
         updateUser();
-    }, [])
+    }, []);
+
+    useEffect( () => {
+        if (user?.id) {
+            updatePost();
+        }
+    }, [user, updatePost]);
 
     return (
         <div>
@@ -27,6 +56,15 @@ function FetchCard() {
             </button>
             <h1>{user.name}</h1>
             <h1>{user.email}</h1>
+
+            <br />
+
+            <h2>Posts - user: {user.id}</h2>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
