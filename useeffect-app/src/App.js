d4f1f01aa1cecbf814b8a11ef83a333ac04754ef
Abel AR <abel_john_14@hotmail.com>
2021-02-18 12:50:36 -0500

    Add clean up

--- useeffect-app/src/App.js
+++ useeffect-app/src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import ResizeApp from './ResizeApp';
+// import ResizeApp from './ResizeApp';
 // import FetchCard from './FetchCard';
-// import LifeCycle from './LifeCycle';
+import LifeCycle from './LifeCycle';
 
 function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div >
-      {/* <LifeCycle /> */}
-      {/* <FetchCard /> */}
       <button onClick = {() => setShow(!show)}>
         Show/Hidde
       </button>
-      { show && <ResizeApp />}
+      {show && <LifeCycle />}
+      {/* <FetchCard /> */}
+      {/* { show && <ResizeApp />} */}
     </div>
   );
 }
--- useeffect-app/src/LifeCycle.js
+++ useeffect-app/src/LifeCycle.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+console.log('Pre-render');
 
 function LifeCycle() {
+    console.log('Logic render');
+
     const [firstCounter, setFirstCounter] = useState(0);
     const [secondCounter, setSecondCounter] = useState(0);
 
     useEffect(() => {
-        console.log('useEffect []')
+        console.log('useEffect []');
+        return () => {
+            console.log('Cleanup useEffect []');
+        }
     }, []);
 
     useEffect(() => {
         console.log('UseEfect no dependency');
+        return () => {
+            console.log('Cleanup useEffect no dependency');
+        }
     });
 
     useEffect(()=> {
-        console.log(`useEffect first counter [${firstCounter}]`)
+        console.log(`useEffect first counter [${firstCounter}]`);
+        return () => {
+            console.log('Cleanup useEffect first counter');
+        }
     }, [firstCounter]);
 
     useEffect(() => {
-        console.log(`useEffect second counter [${secondCounter}]`)
+        console.log(`useEffect second counter [${secondCounter}]`);
+        return () => {
+            console.log('Cleanup useEffect second counter');
+        }
     }, [secondCounter]);
 
     useEffect(() => {
         console.log(`useEffect [${firstCounter}, ${secondCounter}]`)
+        return () => {
+            console.log('Cleanup useEffect both counters');
+        }
     }, [firstCounter, secondCounter])
 
     return (
         <div>
+            {console.log('Return render')}
             <h1>Click First Counter: {firstCounter}</h1>
             <h1>Click Second Counter: {secondCounter}</h1>
 
